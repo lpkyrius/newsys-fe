c67327316a6a289a44e31dbd63a0a0248582a555
lpkyrius <lpkyrius@hotmail.com>
2023-08-17 18:05:42 +0100

    Intial commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0 '>
+            <Tilt className='Tilt br-2 shadow-2' style={{ height: '80px', width: '160px' }}>
+                <div className='Tilt-inner' class="vertical-center center">RCCBRASIL</div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
