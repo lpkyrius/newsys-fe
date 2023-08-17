d32be83a4e3c33bb482ff5758fbc3bbe7a39a3f4
lpkyrius <lpkyrius@hotmail.com>
2023-08-17 21:43:50 +0100

    Initial components Navigation and Logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
+import React, { Component } from "react";
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import './App.css';
+import Register from "./components/Register/Register";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/* {
-      <Login />} */}
-    </div>
-  );
+const initialState = {
+  input: '',
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        joined: '' 
+      }
 }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'signin'
+    }
+  }
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state; 
+    return (
+      <div className="App">
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        <Logo />
+        {   route === 'home' 
+          ? <div>
+              
+            </div>
+          : (
+              route === 'signin'
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
+      </div>
+    );
+  }  
+}
+
+
+//   /////////////
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navigation />
+//       <Signin />
+//       <Logo />
+//       {/* {
+//       <Login />} */}
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     return (
         <div className='ma4 mt0 '>
             <Tilt className='Tilt br-2 shadow-2' style={{ height: '80px', width: '160px' }}>
-                <div className='Tilt-inner' class="vertical-center center">RCCBRASIL</div>
+                <div className='Tilt-inner' class="vertical-center center"><span>RCC</span>BRASIL</div>
             </Tilt>
         </div>
     );
