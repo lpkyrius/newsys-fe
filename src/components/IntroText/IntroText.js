8271f31dc7c701412d403e5faf9e485e48f4ef8a
lpkyrius <lpkyrius@hotmail.com>
2023-09-22 09:12:13 +0100

    Updated intro text

--- src/components/IntroText/IntroText.js
+++ src/components/IntroText/IntroText.js
@@ -5,8 +5,8 @@ const IntroText = () => {
     return (
         <div className="MainText lineUp" data-scrollax-parent="true">
             <span className="f4 link white">Welcome</span>
-            <h1 className="f1 link white">Information is power only if you can access & take action with it.</h1>
-            <h6 className="f5 link white">Log in or Register to access your data.</h6>
+            <h1 className="f1 link white">Information is power only if you can access & take action with it</h1>
+            <h6 className="f5 link white">Log in or Register to access your data</h6>
         </div>
     );
 }
