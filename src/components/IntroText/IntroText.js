36e3c8e4388cb6449463e25bd02f65749c653e77
lpkyrius <lpkyrius@hotmail.com>
2023-09-26 12:40:03 +0100

    Added link to API Swagger documentation

--- src/components/IntroText/IntroText.js
+++ src/components/IntroText/IntroText.js
@@ -8,7 +8,7 @@ const IntroText = () => {
             <h1 className="f1 link white">Information is power only if you can access & take action with it</h1>
             <h6 className="f5 link white">Log in or Register to access your data</h6>
             <br></br>
-            <a href={`${ process.env.REACT_APP_SERVER_ADDRESS }api-docs`} className="f6 link white grow db" target="_blank" rel="noreferrer noopener">APIs Documentation</a>
+            <a href={`${ process.env.REACT_APP_SERVER_ADDRESS }api-docs`} className="f6 link white grow db" target="_blank" rel="noreferrer noopener">📝 APIs Documentation</a>
         </div>
     );
 }
