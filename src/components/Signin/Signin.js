3de3e13cda15adbd489d6ccd620bc37a2b58b9c1
lpkyrius <lpkyrius@hotmail.com>
2023-10-19 11:34:29 +0100

    update signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -35,15 +35,15 @@ class Signin extends React.Component {
                 password: this.state.signInPassword,
             }),
         })
-            .then((response) => response.json())
-            .then((user) => {
-                if (user.id) {
-                    this.props.loadUser(user);
-                    this.props.onRouteChange('home');
-                } else {
-                    notify('info', user.error);
-                }
-            });
+        .then((response) => response.json())
+        .then((user) => {
+            if (user.id) {
+                this.props.loadUser(user);
+                this.props.onRouteChange('home');
+            } else {
+                notify('info', user.error);
+            }
+        });
     }
 
     render() {
